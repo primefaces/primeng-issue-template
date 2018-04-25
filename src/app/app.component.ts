26e499e58d74e4ccf512fb3b561e5c441f8f5c92
Çağatay Çivici <jagatai@Cagatays-MacBook-Pro.local>
2018-04-25 08:24:50 +0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+
+  counter = 0;
+
+  onClick() {
+    this.counter++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,149 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppComponent }   from './app.component';
 
-import { AppComponent } from './app.component';
-
+// Import PrimeNG modules
+import {AccordionModule} from 'primeng/primeng';
+import {AutoCompleteModule} from 'primeng/primeng';
+import {BreadcrumbModule} from 'primeng/primeng';
+import {ButtonModule} from 'primeng/primeng';
+import {CalendarModule} from 'primeng/primeng';
+import {CarouselModule} from 'primeng/primeng';
+import {ChartModule} from 'primeng/primeng';
+import {CheckboxModule} from 'primeng/primeng';
+import {ChipsModule} from 'primeng/primeng';
+import {CodeHighlighterModule} from 'primeng/primeng';
+import {ConfirmDialogModule} from 'primeng/primeng';
+import {SharedModule} from 'primeng/primeng';
+import {ContextMenuModule} from 'primeng/primeng';
+import {DataGridModule} from 'primeng/primeng';
+import {DataListModule} from 'primeng/primeng';
+import {DataScrollerModule} from 'primeng/primeng';
+import {DataTableModule} from 'primeng/primeng';
+import {DialogModule} from 'primeng/primeng';
+import {DragDropModule} from 'primeng/primeng';
+import {DropdownModule} from 'primeng/primeng';
+import {EditorModule} from 'primeng/primeng';
+import {FieldsetModule} from 'primeng/primeng';
+import {FileUploadModule} from 'primeng/primeng';
+import {GalleriaModule} from 'primeng/primeng';
+import {GMapModule} from 'primeng/primeng';
+import {GrowlModule} from 'primeng/primeng';
+import {InputMaskModule} from 'primeng/primeng';
+import {InputSwitchModule} from 'primeng/primeng';
+import {InputTextModule} from 'primeng/primeng';
+import {InputTextareaModule} from 'primeng/primeng';
+import {LightboxModule} from 'primeng/primeng';
+import {ListboxModule} from 'primeng/primeng';
+import {MegaMenuModule} from 'primeng/primeng';
+import {MenuModule} from 'primeng/primeng';
+import {MenubarModule} from 'primeng/primeng';
+import {MessagesModule} from 'primeng/primeng';
+import {MultiSelectModule} from 'primeng/primeng';
+import {OrderListModule} from 'primeng/primeng';
+import {OverlayPanelModule} from 'primeng/primeng';
+import {PaginatorModule} from 'primeng/primeng';
+import {PanelModule} from 'primeng/primeng';
+import {PanelMenuModule} from 'primeng/primeng';
+import {PasswordModule} from 'primeng/primeng';
+import {PickListModule} from 'primeng/primeng';
+import {ProgressBarModule} from 'primeng/primeng';
+import {RadioButtonModule} from 'primeng/primeng';
+import {RatingModule} from 'primeng/primeng';
+import {ScheduleModule} from 'primeng/primeng';
+import {SelectButtonModule} from 'primeng/primeng';
+import {SlideMenuModule} from 'primeng/primeng';
+import {SliderModule} from 'primeng/primeng';
+import {SpinnerModule} from 'primeng/primeng';
+import {SplitButtonModule} from 'primeng/primeng';
+import {StepsModule} from 'primeng/primeng';
+import {TabMenuModule} from 'primeng/primeng';
+import {TabViewModule} from 'primeng/primeng';
+import {TableModule} from 'primeng/table';
+import {TerminalModule} from 'primeng/primeng';
+import {TieredMenuModule} from 'primeng/primeng';
+import {ToggleButtonModule} from 'primeng/primeng';
+import {ToolbarModule} from 'primeng/primeng';
+import {TooltipModule} from 'primeng/primeng';
+import {TreeModule} from 'primeng/primeng';
+import {TreeTableModule} from 'primeng/primeng';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    AutoCompleteModule,
+    BreadcrumbModule,
+    ButtonModule,
+    CalendarModule,
+    CarouselModule,
+    ChartModule,
+    CheckboxModule,
+    ChipsModule,
+    CodeHighlighterModule,
+    ConfirmDialogModule,
+    SharedModule,
+    ContextMenuModule,
+    DataGridModule,
+    DataListModule,
+    DataScrollerModule,
+    DataTableModule,
+    DialogModule,
+    DragDropModule,
+    DropdownModule,
+    EditorModule,
+    FieldsetModule,
+    FileUploadModule,
+    GalleriaModule,
+    GMapModule,
+    GrowlModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    LightboxModule,
+    ListboxModule,
+    MegaMenuModule,
+    MenuModule,
+    MenubarModule,
+    MessagesModule,
+    MultiSelectModule,
+    OrderListModule,
+    OverlayPanelModule,
+    PaginatorModule,
+    PanelModule,
+    PanelMenuModule,
+    PasswordModule,
+    PickListModule,
+    ProgressBarModule,
+    RadioButtonModule,
+    RatingModule,
+    ScheduleModule,
+    SelectButtonModule,
+    SlideMenuModule,
+    SliderModule,
+    SpinnerModule,
+    SplitButtonModule,
+    StepsModule,
+    TableModule,
+    TabMenuModule,
+    TabViewModule,
+    TerminalModule,
+    TieredMenuModule,
+    ToggleButtonModule,
+    ToolbarModule,
+    TooltipModule,
+    TreeModule,
+    TreeTableModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
