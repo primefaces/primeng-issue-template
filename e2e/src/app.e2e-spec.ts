60c28f5337daa01d411035b92aadcaa3c205e684
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-08-12 11:05:03 +0300

    Update versions

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,4 @@ describe('demo App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
 });
