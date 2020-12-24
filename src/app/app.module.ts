e9ba7f27aa95a9221995a1a9e372beec43514e61
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-12-24 17:28:36 +0300

    version update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { AppComponent }   from './app.component';
 // Import PrimeNG modules
 import { AccordionModule } from 'primeng/accordion';
 import { AutoCompleteModule } from 'primeng/autocomplete';
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { CarouselModule } from 'primeng/carousel';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
 import { ChartModule } from 'primeng/chart';
 import { CheckboxModule } from 'primeng/checkbox';
+import { ChipModule } from 'primeng/chip';
 import { ChipsModule } from 'primeng/chips';
 import { CodeHighlighterModule } from 'primeng/codehighlighter';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
@@ -23,6 +27,7 @@ import { ContextMenuModule } from 'primeng/contextmenu';
 import { DataViewModule } from 'primeng/dataview';
 import { VirtualScrollerModule } from 'primeng/virtualscroller';
 import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
 import { DragDropModule } from 'primeng/dragdrop';
 import { DropdownModule } from 'primeng/dropdown';
 import { DynamicDialogModule } from 'primeng/dynamicdialog';
@@ -37,6 +42,7 @@ import { InputSwitchModule } from 'primeng/inputswitch';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { KnobModule } from 'primeng/knob';
 import { LightboxModule } from 'primeng/lightbox';
 import { ListboxModule } from 'primeng/listbox';
 import { MegaMenuModule } from 'primeng/megamenu';
@@ -57,15 +63,19 @@ import { RadioButtonModule } from 'primeng/radiobutton';
 import { RatingModule } from 'primeng/rating';
 import { FullCalendarModule } from 'primeng/fullcalendar';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { ScrollTopModule } from 'primeng/scrolltop';
 import { SelectButtonModule } from 'primeng/selectbutton';
+import { SkeletonModule } from 'primeng/skeleton';
 import { SlideMenuModule } from 'primeng/slidemenu';
 import { SliderModule } from 'primeng/slider';
 import { SpinnerModule } from 'primeng/spinner';
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { SplitterModule } from 'primeng/splitter';
 import { StepsModule } from 'primeng/steps';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { TableModule } from 'primeng/table';
 import { TabViewModule } from 'primeng/tabview';
+import { TagModule } from 'primeng/tag';
 import { TerminalModule } from 'primeng/terminal';
 import { TieredMenuModule } from 'primeng/tieredmenu';
 import { ToastModule } from 'primeng/toast';
@@ -77,19 +87,23 @@ import { TreeTableModule } from 'primeng/treetable';
 
 @NgModule({
   imports: [
+    AvatarModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     AccordionModule,
     AutoCompleteModule,
+    BadgeModule,
     BreadcrumbModule,
     ButtonModule,
     CalendarModule,
     CarouselModule,
+    CascadeSelectModule,
     ChartModule,
     CheckboxModule,
     ChipsModule,
+    ChipModule,
     CodeHighlighterModule,
     ColorPickerModule,
     ConfirmDialogModule,
@@ -97,6 +111,7 @@ import { TreeTableModule } from 'primeng/treetable';
     VirtualScrollerModule,
     DataViewModule,
     DialogModule,
+    DividerModule,
     DragDropModule,
     DropdownModule,
     DynamicDialogModule,
@@ -111,6 +126,7 @@ import { TreeTableModule } from 'primeng/treetable';
     InputTextModule,
     InputTextareaModule,
     InputNumberModule,
+    KnobModule,
     LightboxModule,
     ListboxModule,
     MegaMenuModule,
@@ -132,14 +148,18 @@ import { TreeTableModule } from 'primeng/treetable';
     FullCalendarModule,
     SelectButtonModule,
     ScrollPanelModule,
+    ScrollTopModule,
+    SkeletonModule,
     SlideMenuModule,
     SliderModule,
     SpinnerModule,
+    SplitterModule,
     SplitButtonModule,
     StepsModule,
     TableModule,
     TabMenuModule,
     TabViewModule,
+    TagModule,
     TerminalModule,
     TieredMenuModule,
     ToastModule,
